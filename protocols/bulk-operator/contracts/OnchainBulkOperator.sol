b8b2c781c3ae57a86ebd9520fbc5f7926760308d
turinglabsorg <sebastiano.cataudo@gmail.com>
2022-11-15 08:56:48 +0100

    preparing workplace for bulk operator

---
+++ protocols/bulk-operator/contracts/OnchainBulkOperator.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.6;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ * @title OnchainBulkOperator
+ */
+contract OnchainBulkOperator is Ownable {
+   
+}
